0c074cbb4acc1a5457078e8fe6ff349e4068ec8f
chaborz2005 <diyorabdunaimov@gmail.com>
2024-09-07 12:09:05 +0500

    my first script in first week.

---
+++ Week1/hello.py
@@ -0,0 +1,32 @@
+#ism = "diyor"
+#print(ism) 
+#ism = "daler"
+#print(ism)
+
+#a = 6
+#b = 7
+#c = (a+b)**2
+#print(c)
+
+#ism = "diyor"
+#shahar = "kykon"
+#print("результат:", 5 ** 3)
+ 
+
+#number = 5
+#print("результат", number)
+
+#number = 8
+#print("результат", number)
+
+#print(number + number)
+
+num1 = int(input("Введите первое число :"))
+num2 = int(input("Введите второе число :"))
+
+print("Result:", num1 + num2)
+print("Result:", num1 - num2)
+print("Result:", num1 * num2)
+print("Result:", num1 / num2)
+
+
